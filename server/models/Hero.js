52b690bf853cbb18cc70d0fa56513fe3e9c6f502
zihaoz12 <zzhaa12@gmail.com>
2019-10-28 23:54:27 -0700

    set up Hero model

--- server/models/Hero.js
+++ server/models/Hero.js
@@ -6,7 +6,62 @@ const HeroSchema = new mongoose.Schema({
     },
     avatar:{
         type:String
-    }
+    },
+    title:{
+        type:String
+    },
+    categories: [{
+        type: mongoose.SchemaTypes.ObjectId, ref: 'Category'
+    }],
+    scores:{
+        difficult:{
+            type: Number
+        },
+        skills: {
+            type: Number
+        },
+        attack: {
+            type: Number
+        },
+        survive: {
+            type: Number
+        }
+    },
+    skills:[{
+        icon: {
+            type:String
+        },
+        name: {
+            type:String
+        },
+        description: {
+            type:String
+        },
+        tips: {
+            type:String
+        }
+    }],
+    items1:[{
+        type: mongoose.SchemaTypes.ObjectId, ref: 'Item'
+    }],
+    items2:[{
+        type: mongoose.SchemaTypes.ObjectId, ref: 'Item'
+    }],
+    usageTips:{
+        type: String
+    },
+    teamTips:{
+        type: String
+    },
+    partners:[{
+        hero:{
+            type: mongoose.SchemaTypes.ObjectId, ref: 'Hero'
+        },
+        description:{
+            type: String
+        }
+    }]
+
 })
 
 module.exports =  mongoose.model('Hero',HeroSchema)
