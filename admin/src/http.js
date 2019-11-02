7a73cf967998ebdd10f4b0b2f5d4f220ecdb8463
zihaoz12 <zzhaa12@gmail.com>
2019-11-01 17:21:13 -0700

    add Login Auth, resource middleware

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import Vue from 'vue'
+import router from './router'
 
 
 const http = axios.create({
@@ -7,8 +8,19 @@ const http = axios.create({
 })
 
 
-//http response
+// Add a request interceptor
+http.interceptors.request.use(function (config) {
+    // Do something before request is sent // add a sapce after Bearer
+    if(localStorage.token){
+        config.headers.Authorization = 'Bearer ' + localStorage.token
+    }
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
 
+//http response interceptor
 http.interceptors.response.use( res => {
     return res
 }, err => {
@@ -17,8 +29,15 @@ http.interceptors.response.use( res => {
             type: 'error',
             message: err.response.data.message
         })     
-        return Promise.reject(err)
+       
+        // equal to back-end's error code
+        //if error redirect back to login
+        if(err.response.data.statusCode === 401){
+            router.push('/login')
+        }
     }
+
+    return Promise.reject(err)
 })
 
 export default http
